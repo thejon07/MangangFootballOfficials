c9a4508d488759754f22298f8e0bf1da7623895e
thejon7 <148666144+thejon07@users.noreply.github.com>
2024-10-27 10:51:02 +0600

    Update vite.config.js

--- Mangangfootballofficials/vite.config.js
+++ Mangangfootballofficials/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     proxy: {
       // Proxy all requests starting with '/users' to your backend server
       "/users": {
-        target: "http://localhost:4000",  // Your backend server
+        target: "https://dreamers-backend-git-master-bijons-projects-01181d74.vercel.app/",  // Your backend server
         changeOrigin: true,  // Needed for virtual hosted sites
         rewrite: (path) => path.replace(/^\/users/, "/users"),  // Optional if the backend expects the same '/users' prefix
       }
