c8f8aa7ed5299cbf4e9463529c5b960f062289f0
thejon7 <148666144+thejon07@users.noreply.github.com>
2024-10-28 21:24:17 +0600

    Update handleaxios.js

--- Mangangfootballofficials/src/utils/handleaxios.js
+++ Mangangfootballofficials/src/utils/handleaxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance
 const instance = axios.create({
-  baseURL: "https://dreamersbackend.onrender.com" // Use Vite's method to access environment variables
+  baseURL: "https://dreamersbackend.onrender.com" || "http://localhost:4000" // Use Vite's method to access environment variables
 });
 
 // Add Axios Interceptor to handle token refresh
